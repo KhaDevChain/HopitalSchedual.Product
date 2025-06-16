9e88e603ae00b797fe7b521a4e282003aba834e9
khann.devops <khann.devops.159@gmail.com>
2025-06-16 11:40:56 +0700

    Change value in Blood Enum

--- src/main/java/com/az9s/hopital/Backend/utils/option/BloodEnum.java
+++ src/main/java/com/az9s/hopital/Backend/utils/option/BloodEnum.java
@@ -1,8 +1,12 @@
 package com.az9s.hopital.Backend.utils.option;
 
 public enum BloodEnum {
-    A,
-    B,
-    AB,
-    O
+    A_POSITIVE,
+    A_NEGATIVE,
+    B_POSITIVE,
+    B_NEGATIVE,
+    AB_POSITIVE,
+    AB_NEGATIVE,
+    O_POSITIVE,
+    O_NEGATIVE 
 }
